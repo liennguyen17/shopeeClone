1137e941d4345e26e0c61928100feebdd442efab
liennguyen17 <nguyenthilien12a8bn@gmail.com>
2024-01-10 11:21:26 +0700

    custom class .container

--- ShopeeClone/src/components/Footer/Footer.tsx
+++ ShopeeClone/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='py-16 bg-neutral-100'>
-      <div className='max-w-7xl mx-auto px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
           <div className='lg:col-span-1'>
             <div>© 2024 Shopee. Tất cả các quyền được bảo lưu.</div>
--- ShopeeClone/tailwind.config.cjs
+++ ShopeeClone/tailwind.config.cjs
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
@@ -8,5 +14,17 @@ export default {
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
 }
