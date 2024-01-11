8e882be5588f64a540c957974db40f24ac283d43
liennguyen17 <nguyenthilien12a8bn@gmail.com>
2024-01-11 22:37:02 +0700

    167 - error message with axios interceptor

--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -1,9 +1,15 @@
+import { ToastContainer } from 'react-toastify'
 import useRouteElements from './useRouteElements'
-
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   const routerElements = useRouteElements()
 
-  return <div>{routerElements}</div>
+  return (
+    <div>
+      {routerElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
--- ShopeeClone/src/utils/http.ts
+++ ShopeeClone/src/utils/http.ts
@@ -1,4 +1,5 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { HttpStatusCode, AxiosError, type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
 
 class Http {
   instanse: AxiosInstance
@@ -10,6 +11,21 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+
+    this.instanse.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
