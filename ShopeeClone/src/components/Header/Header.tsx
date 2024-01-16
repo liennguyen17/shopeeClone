4831f256f50de1d07bdeb7867beeea7904ef00c4
liennguyen17 <nguyenthilien12a8bn@gmail.com>
2024-01-16 14:27:59 +0700

    172-Ui Popover cart

--- ShopeeClone/src/components/Header/Header.tsx
+++ ShopeeClone/src/components/Header/Header.tsx
@@ -114,8 +114,120 @@ export default function Header() {
               </button>
             </div>
           </form>
-          <div className='cols-span-1'>
-            <svg
+          <div className='cols-span-1 justify-self-end'>
+            <Popover
+              initialOpen
+              renderPopover={
+                <div className='bg-white relative shadow-md rounded-sm border border-gray-200 max-w-[400px] text-sm'>
+                  <div className='p-2'>
+                    <div className='text-gray-400 capitalize'>Sản phẩm mới thêm</div>
+                    <div className='mt-5'>
+                      <div className='mt-4 flex'>
+                        <div className='flex-shrink-0'>
+                          <img
+                            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lh34j7igr01fea_tn'
+                            alt='anh'
+                            className='w-11 h-11 object-cover'
+                          />
+                        </div>
+                        {/* truncate: neu ten dai thi no se ...  ket hop voi overflow-hidden*/}
+                        <div className='flex-grow ml-2 overflow-hidden'>
+                          <div className='truncate'>
+                            Siêu Son Dưỡng Có Màu Thuần Chay Môi Nhạy Cảm Glamrr Q Vega n Super Lip Balm (4.5g)
+                          </div>
+                        </div>
+                        <div className='ml-2 flex-shrink-0'>
+                          <span className='text-orange'>₫199.000</span>
+                        </div>
+                      </div>
+                      <div className='mt-4 flex'>
+                        <div className='flex-shrink-0'>
+                          <img
+                            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lh34j7igr01fea_tn'
+                            alt='anh'
+                            className='w-11 h-11 object-cover'
+                          />
+                        </div>
+                        {/* truncate: neu ten dai thi no se ...  ket hop voi overflow-hidden*/}
+                        <div className='flex-grow ml-2 overflow-hidden'>
+                          <div className='truncate'>
+                            Siêu Son Dưỡng Có Màu Thuần Chay Môi Nhạy Cảm Glamrr Q Vega n Super Lip Balm (4.5g)
+                          </div>
+                        </div>
+                        <div className='ml-2 flex-shrink-0'>
+                          <span className='text-orange'>₫199.000</span>
+                        </div>
+                      </div>
+                      <div className='mt-4 flex'>
+                        <div className='flex-shrink-0'>
+                          <img
+                            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lh34j7igr01fea_tn'
+                            alt='anh'
+                            className='w-11 h-11 object-cover'
+                          />
+                        </div>
+                        {/* truncate: neu ten dai thi no se ...  ket hop voi overflow-hidden*/}
+                        <div className='flex-grow ml-2 overflow-hidden'>
+                          <div className='truncate'>
+                            Siêu Son Dưỡng Có Màu Thuần Chay Môi Nhạy Cảm Glamrr Q Vega n Super Lip Balm (4.5g)
+                          </div>
+                        </div>
+                        <div className='ml-2 flex-shrink-0'>
+                          <span className='text-orange'>₫199.000</span>
+                        </div>
+                      </div>
+                      <div className='mt-4 flex'>
+                        <div className='flex-shrink-0'>
+                          <img
+                            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lh34j7igr01fea_tn'
+                            alt='anh'
+                            className='w-11 h-11 object-cover'
+                          />
+                        </div>
+                        {/* truncate: neu ten dai thi no se ...  ket hop voi overflow-hidden*/}
+                        <div className='flex-grow ml-2 overflow-hidden'>
+                          <div className='truncate'>
+                            Siêu Son Dưỡng Có Màu Thuần Chay Môi Nhạy Cảm Glamrr Q Vega n Super Lip Balm (4.5g)
+                          </div>
+                        </div>
+                        <div className='ml-2 flex-shrink-0'>
+                          <span className='text-orange'>₫199.000</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='flex mt-6 items-center justify-between'>
+                      <div className='capitalize text-xs text-gray-500'>Thêm Hàng Vào Giỏ</div>
+                      <button className='capitalize bg-orange hover:bg-opacity-90 px-4 py-2 rounded-sm text-white'>
+                        Xem Giỏ Hàng
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              }
+            >
+              <Link to='/' className='relative'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='h-8 w-8'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+                  />
+                </svg>
+                {/* {purchasesInCart && purchasesInCart.length > 0 && (
+                  <span className='absolute top-[-5px] left-[17px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange '>
+                    {purchasesInCart?.length}
+                  </span>
+                )} */}
+              </Link>
+            </Popover>
+            {/* <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
@@ -128,7 +240,7 @@ export default function Header() {
                 strokeLinejoin='round'
                 d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
               />
-            </svg>
+            </svg> */}
           </div>
         </div>
       </div>
