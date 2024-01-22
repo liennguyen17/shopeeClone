727044299c8bd836e64ec9603056f120f1db9210
liennguyen17 <nguyenthilien12a8bn@gmail.com>
2024-01-22 15:49:28 +0700

    179-UI Product Component

--- ShopeeClone/src/pages/ProductList/Product/Product.tsx
+++ ShopeeClone/src/pages/ProductList/Product/Product.tsx
@@ -1,5 +1,73 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Product() {
-  return <div>Product</div>
+  return (
+    <Link to='/'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lq1pm073fmxec4_tn'
+            alt=''
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-2 overflow-hidden'>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>
+            Tai Nghe True Wireless OPPO Enco Air 2 Pro | Hiệu Ứng Âm Thanh Enco Live - Bán kèm Phone
+          </div>
+          <div className='flex items-center mt-3'>
+            <div className='line-throungh max-w-[50%] text-gray-500 truncate'>
+              <span className='text-xs'>₫</span>
+              <span>1.990.000</span>
+            </div>
+            <div className='text-orange truncate ml-1'>
+              <span className='text-xs'>₫</span>
+              <span>1.290.000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-end'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
+                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
+                  >
+                    <polygon
+                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
+                </div>
+                <svg
+                  enableBackground='new 0 0 15 15'
+                  viewBox='0 0 15 15'
+                  x={0}
+                  y={0}
+                  className='w-3 h-3 fill-current text-gray-300'
+                >
+                  <polygon
+                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeMiterlimit={10}
+                  />
+                </svg>
+              </div>
+              <div className='ml-2 text-sm'>
+                <span>Đã bán</span>
+                <span className='ml-1'>23.5k</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
